17900780a3be31f6f9d97703ecf8a503e0188e29
惟恺 朱 <veikai@126.com>
2020-03-31 09:07:11 +0800

    代码提交

--- example.py
+++ example.py
@@ -3,8 +3,10 @@ from PyWeChatSpy import WeChatSpy
 
 def parser(data):
     if data["type"] == 1:
+        # 微信登录信息
         print(data)
     elif data["type"] == 5:
+        # 微信消息
         for i in data["data"]:
             print(i)
     else:
