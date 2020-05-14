730ef0387dd471bdb152e48bbec1df060cf9e83a
惟恺 朱 <veikai@126.com>
2020-05-14 16:56:21 +0800

    1.0.3.5 1.新增创建群聊功能 2.新增分享群聊功能

--- example.py
+++ example.py
@@ -25,25 +25,11 @@ def parser(data):
         # 消息
         for item in data["data"]:
             print(item)
-            if item["msg_type"] == 1:
-                # 普通聊天消息
-                wxid1, wxid2 = item["wxid1"], item.get("wxid2")
-                # reply = get_reply(item["content"])
-                # spy.send_text(wxid1, reply)
-                if contact := contact_dict.get(wxid1):
-                    print(contact)
-                else:
-                    spy.query_contact_details(wxid1)
-                if wxid1.endswith("chatroom") and wxid2:
-                    if contact := contact_dict.get(wxid2):
-                        print(contact)
-                    else:
-                        spy.query_contact_details(wxid2, wxid1)
-            elif item["msg_type"] == 37:
+            if item["msg_type"] == 37:
                 # 好友请求消息
                 obj = etree.XML(item["content"])
                 encryptusername, ticket = obj.xpath("/msg/@encryptusername")[0], obj.xpath("/msg/@ticket")[0]
-                spy.accept_new_contact(encryptusername, ticket)
+                # spy.accept_new_contact(encryptusername, ticket)
     elif data["type"] == 2:
         # 联系人详情
         print(data)
@@ -57,5 +43,4 @@ def parser(data):
 
 if __name__ == '__main__':
     spy = WeChatSpy(parser=parser)
-    spy.add_log_output_file()  # 添加日志输出文件
     spy.run()
