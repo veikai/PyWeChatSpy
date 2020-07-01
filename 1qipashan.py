f2ee6b876aee11dcd3c017d1a30812fd3641c46d
惟恺 朱 <veikai@126.com>
2020-07-01 19:21:36 +0800

    例子提交

---
+++ 1qipashan.py
@@ -0,0 +1,38 @@
+from PyWeChatSpy import WeChatSpy
+from tencentcloud.common import credential
+from tencentcloud.ocr.v20181119 import ocr_client, models
+import base64
+
+
+image_path = r"D:\18020891\GitHub\PyWeChatSpy\images\bqstz.jpg"
+
+
+def my_parser(data):
+    if data["type"] == 5:  # 判断是微信消息数据
+        for msg in data["data"]:  # 遍历微信消息
+            if not msg["self"]:  # 判断消息是否由他人发出
+                if msg["msg_type"] == 1:  # 判断是否文本消息
+                    print(msg["content"])
+                    if "爬山" in msg["content"]:  # 判断是否去爬山
+                        spy.send_file(msg["wxid1"], image_path)  # 发送图片
+                elif msg["msg_type"] == 3:  # 判断是否图片消息
+                    req = models.GeneralAccurateOCRRequest()
+                    with open(msg["image_path"], "rb") as rf:  # 读取图片
+                        base64_data = base64.b64encode(rf.read())  # 转化为base64
+                        req.ImageBase64 = base64_data.decode()
+                    cred = credential.Credential("AKIDe4DxkJXlsiHZnlDwFINBixHKJ4gJwLyv",
+                                                 "py1ZrtfAWTGn3MQFnCQ0LQaU8WorHTkl")
+                    client = ocr_client.OcrClient(cred, "ap-shanghai")
+                    r = client.GeneralAccurateOCR(req)  # 识别图片
+                    for item in r.TextDetections:
+                        print(item.DetectedText)
+                        if "爬山" in item.DetectedText:
+                            spy.send_file(msg["wxid1"], image_path)  # 发送图片
+                            break
+
+
+spy = WeChatSpy(parser=my_parser)  # 实例化WeChatSpy类
+
+
+if __name__ == '__main__':
+    spy.run()  # 运行代码
---
+++ pai1pai.py
@@ -0,0 +1,22 @@
+from PyWeChatSpy import WeChatSpy
+import random
+import re
+
+
+def my_parser(data):
+    if data["type"] == 5:  # 判断是微信消息数据
+        for msg in data["data"]:  # 遍历微信消息
+            if msg["msg_type"] == 10000:  # 判断是微信拍一拍系统提示
+                # 因为微信系统消息很多 因此需要用正则匹配消息内容进一步过滤拍一拍提示
+                # {'self': 0, 'msg_type': 10000, 'wxid1': '179xxxxxx72@chatroom', 'content': '"Mandy的小脑袋" 拍了拍你'}
+                m = re.search('".*" 拍了拍你', msg["content"])
+                if m:  # 搜索到了匹配的字符串 判断为拍一拍
+                    image_path = f"images/{random.randint(1, 7)}.jpg"  # 随机选一张回复用的图片
+                    spy.send_file(msg["wxid1"], image_path)  # 发送图片
+
+
+spy = WeChatSpy(parser=my_parser)  # 实例化WeChatSpy类
+
+
+if __name__ == '__main__':
+    spy.run()  # 运行代码
