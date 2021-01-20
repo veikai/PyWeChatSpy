72d90ce8d6f4f4e006781828f9264587bde08db9
veikai <veikai@126.com>
2021-01-20 15:33:04 +0800

    更新拍一拍示例代码

--- pai1pai.py
+++ pai1pai.py
@@ -22,7 +22,7 @@ def parse():
                 print(_type, _from, _to, content)
                 if _type == 10000:  # 判断是微信拍一拍系统提示
                     # 因为微信系统消息很多 因此需要用正则匹配消息内容进一步过滤拍一拍提示
-                    m = re.search('".*" 拍了拍你', content)
+                    m = re.search('".*" 拍了拍我', content)
                     if m:  # 搜索到了匹配的字符串 判断为拍一拍
                         image_path = f"images/{random.randint(1, 7)}.jpg"  # 随机选一张回复用的图片
                         spy.send_file(_from, image_path)  # 发送图片
